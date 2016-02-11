372e6b0fd5fdc2f2378f4f00d869847b8e21ddd6
Dave Willmer <dave.willmer@gmail.com>
2016-02-11 18:31:18 +0000

    Add messaging

--- src/webpack.conf.js
+++ src/webpack.conf.js
@@ -2,7 +2,7 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     filename: './lib/bundle.js',
-    libraryTarget: "var",
+    libraryTarget: "this",
     library: "bokeh_phosphor"
   },
   resolve: {
